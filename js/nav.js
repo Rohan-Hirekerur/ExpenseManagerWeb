57eb08c8f2b7dd53dcbab50d039d5f08750b71aa
Rohan Hirekerur <rohanhirekerur@gmail.com>
2018-07-23 11:08:00 +0530

    Animations done

--- js/nav.js
+++ js/nav.js
@@ -2,25 +2,25 @@ $('#btnInc').click(function(){
   $(this).addClass('active');
   $('#btnBal').removeClass('active');
   $('#btnExp').removeClass('active');
-  $('#inpage').removeClass('hide')
   $('#balpage').addClass('hide');
   $('#expage').addClass('hide');
+  $('#inpage').removeClass('hide')
 });
 
 $('#btnBal').click(function(){
   $(this).addClass('active');
   $('#btnInc').removeClass('active');
   $('#btnExp').removeClass('active');
-  $('#balpage').removeClass('hide')
   $('#inpage').addClass('hide');
   $('#expage').addClass('hide');
+  $('#balpage').removeClass('hide')     
 });
 
 $('#btnExp').click(function(){
   $(this).addClass('active');
   $('#btnBal').removeClass('active');
   $('#btnInc').removeClass('active');
-  $('#expage').removeClass('hide')
   $('#balpage').addClass('hide');
   $('#inpage').addClass('hide');
+  $('#expage').removeClass('hide')
 });
